dddfa18d757ca2da4357cd72afab3966aa4528bb
Alexander Steiner <steiale77@gmail.com>
2026-05-02 20:55:58 +0200

    wireguide-plus 1.0.7 fix zip structure

--- Casks/wireguide-plus.rb
+++ Casks/wireguide-plus.rb
@@ -1,6 +1,6 @@
 cask "wireguide-plus" do
   version "1.0.7"
-  sha256 "9b2e706da30aca6f45045dd29353c3c2460b0140ff366c7d77f13b0feaea6eb6"
+  sha256 "2bb4dd79a65f19c8c8a82c1100bdb89ce4fddf9d37c109a31af7c9898536b324"
 
   url "https://github.com/steiale/wireguide/releases/download/v#{version}/wireguide-plus-v#{version}-darwin-universal.zip"
   name "WireGuide+"
