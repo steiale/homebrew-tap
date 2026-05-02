9ec2b044614fb8a2c124b94305143bd3e229b797
Alexander Steiner <steiale77@gmail.com>
2026-05-02 20:27:35 +0200

    cask: wireguide-plus 1.0.5 — about dialog fixes

--- Casks/wireguide-plus.rb
+++ Casks/wireguide-plus.rb
@@ -1,6 +1,6 @@
 cask "wireguide-plus" do
-  version "1.0.4"
-  sha256 "d9b9afb26d66c379d618b5b861aaca5643adecdd7756ce01a173ebda516be5dc"
+  version "1.0.5"
+  sha256 "4ddc22e5fc59928519e531fe74f35473e3e70075c8018e7681c79670a6b7ca49"
 
   url "https://github.com/steiale/wireguide/releases/download/v#{version}/wireguide-plus-v#{version}-darwin-universal.zip"
   name "WireGuide+"
