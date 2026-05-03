be60612e0f6a60e188f13fb7138627edfe14b460
Alex Steiner <alex@macdev.localdomain>
2026-05-03 23:08:25 +0200

    wireguide-plus 1.0.23 fix bundle name in zip

--- Casks/wireguide-plus.rb
+++ Casks/wireguide-plus.rb
@@ -1,6 +1,6 @@
 cask "wireguide-plus" do
   version "1.0.23"
-  sha256 "6feda959e6bee675f7d8da86c15f40276e3b63ef43cbca05cfc3d21f229ca039"
+  sha256 "fae7a593071d6c2c68f93510fbc661c89d537abff93f6017e70490bc36599837"
 
   url "https://github.com/steiale/wireguide/releases/download/v#{version}/wireguide-plus-v#{version}-darwin-arm64.zip"
   name "WireGuide+"
