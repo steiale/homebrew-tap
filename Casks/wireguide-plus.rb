789ffb96a0a09af491d1f2eac1322e1335fd9972
Alex Steiner <alex@macdev.localdomain>
2026-05-03 23:04:24 +0200

    wireguide-plus 1.0.23 (arm64)

--- Casks/wireguide-plus.rb
+++ Casks/wireguide-plus.rb
@@ -1,8 +1,8 @@
 cask "wireguide-plus" do
-  version "1.0.22"
-  sha256 "58558dbf7cace87f6621345b3cbe55793b91c2c3e215b5be092bc2f523de64d3"
+  version "1.0.23"
+  sha256 "6feda959e6bee675f7d8da86c15f40276e3b63ef43cbca05cfc3d21f229ca039"
 
-  url "https://github.com/steiale/wireguide/releases/download/v#{version}/wireguide-plus-v#{version}-darwin-universal.zip"
+  url "https://github.com/steiale/wireguide/releases/download/v#{version}/wireguide-plus-v#{version}-darwin-arm64.zip"
   name "WireGuide+"
   desc "WireGuard VPN desktop client"
   homepage "https://github.com/steiale/wireguide"
