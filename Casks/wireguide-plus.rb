14951720d1bc95f1c6e758b77a6399a84e2d93ab
Alex Steiner <alex@macdev.localdomain>
2026-05-03 23:32:38 +0200

    wireguide-plus 1.0.25

--- Casks/wireguide-plus.rb
+++ Casks/wireguide-plus.rb
@@ -1,6 +1,6 @@
 cask "wireguide-plus" do
-  version "1.0.24"
-  sha256 "2e7eba377d90adc7dbe4541ff1dde78edf6324bee085673672e327aac668c687"
+  version "1.0.25"
+  sha256 "9d487234d9d6c8620c96c448cabf1d6a33b13bf4b92d1fd749b23ffb85a4b7ff"
 
   url "https://github.com/steiale/wireguide/releases/download/v#{version}/wireguide-plus-v#{version}-darwin-arm64.zip"
   name "WireGuide+"
