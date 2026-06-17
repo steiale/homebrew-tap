c4e0473f66fae9294d5f422d9e2890610d38065e
Alex Steiner <alex@steiale.io>
2026-06-17 09:25:24 +0200

    wireguide-plus 1.0.49

--- Casks/wireguide-plus.rb
+++ Casks/wireguide-plus.rb
@@ -1,6 +1,6 @@
 cask "wireguide-plus" do
-  version "1.0.48"
-  sha256 "cf3d03a896e22e5903484ef8758a61c152147f61a91c03a6944a12c1805c1b5d"
+  version "1.0.49"
+  sha256 "8fced9737177018945385571e005016f9e1bf33f253ea255070ca84b9745a249"
 
   url "https://github.com/steiale/wireguide/releases/download/v#{version}/wireguide-plus-v#{version}-darwin-arm64.zip"
   name "WireGuide+"
