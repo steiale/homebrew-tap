e040a7d88893610549b0d033cbb97fe090cb50c4
Alex Steiner <alex@steiale.io>
2026-07-01 07:03:45 +0200

    lockplus: update to v1.0.61

--- Casks/lockplus.rb
+++ Casks/lockplus.rb
@@ -1,6 +1,6 @@
 cask "lockplus" do
-  version "1.0.57"
-  sha256 "0acf4c1a81bdf2439190db7b1ddd5552abc86c02ab0206f193f24128770ca291"
+  version "1.0.61"
+  sha256 "070c9bb83b310bf2521795113092da21f4e05b3f6e8fb239744c8dc5b95fda46"
 
   url "https://github.com/steiale/wireguide/releases/download/v#{version}/lockplus-v#{version}-darwin-arm64.zip"
   name "LockPlus"
